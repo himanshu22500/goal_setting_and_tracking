f03840196912b366d4da3beedea8f44b981e682c
himanshu22500 <himanshu22500@gmail.com>
2024-06-01 00:05:02 +0530

    Add Project files and folder structure.

---
+++ goal_setting_backend/goal_setting_backend/asgi.py
@@ -0,0 +1,18 @@
+"""
+ASGI config for goal_setting_backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "goal_setting_backend.settings"
+)
+
+application = get_asgi_application()
---
+++ goal_setting_core/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GoalSettingCoreConfig(AppConfig):
+    name = "goal_setting_core"
